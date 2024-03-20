b55bcc08b3212d9bb1db42403b215d5a97df416e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-20 21:08:08 +0530

    updated index

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ const io = new Server(server);
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
