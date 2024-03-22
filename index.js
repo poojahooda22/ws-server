d27ba0fcd5872f08e9442c5c77a833286a17133f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-22 21:31:18 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createServer } = require('node:http');
+const { createServer } = require('node:http')
 
 const { join } = require('node:path');
 const { Server } = require('socket.io');
