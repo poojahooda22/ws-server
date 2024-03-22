486f6f60369ae7e098548124baa99fee8032fff8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-22 21:19:03 +0530

    updated index

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg)
   });
-})
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
