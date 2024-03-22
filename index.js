213edf540ebd690692848cf261cbdcfb49737226
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-22 21:19:30 +0530

    updated index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Server } = require('socket.io');
 
 const app = express()
 const server = createServer(app);
-const io = new Server(server)
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
